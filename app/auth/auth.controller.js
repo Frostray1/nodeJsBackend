b02e2fd207ff4702ace44c5d7efbee09881b2946
Artem <a.bychkoff1@yandex.ru>
2023-02-20 15:36:22 +0300

    add register user

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -1,13 +1,47 @@
-
-
 // @desc    Auth user
 // @route   POST /api/auth/login
 // @access  Public
+import { faker } from '@faker-js/faker'
+import asyncHandler from 'express-async-handler'
+import {hash}from "argon2";
+import { prisma } from '../prisma.js'
+import { generateToken } from './generate-token.js'
+import { UserFields } from '../utils/user.utils.js';
 
-import { prisma } from "../prisma.js"
+export const authUser = asyncHandler(async (req, res) => {
+	const user = await prisma.user.findMany()
 
-export const authUser = async (req, res) => {
-    const user = await prisma.user.findMany()
+	res.json(user)
+})
 
-    res.json(user)
-}
+// @desc    register user
+// @route   POST /api/auth/login
+// @access  Public
+
+export const registerUser = asyncHandler(async (req, res) => {
+	const { email, password } = req.body
+
+	const isHaveUser = await prisma.user.findUnique({
+		where: {
+			email
+		}
+	})
+
+	if (isHaveUser) {
+		res.status(400)
+		throw new Error('User already exists')
+	}
+
+	const user = await prisma.user.create({
+		data: {
+			email,
+			password: await hash(password),
+			name: faker.name.fullName()
+		},
+        select: UserFields
+	})
+
+	const token = generateToken(user.id)
+
+	res.json({ user, token })
+})
