44d000c9e888a45a1c60a76c6124ba200ec47207
Artem <a.bychkoff1@yandex.ru>
2023-02-21 14:48:25 +0300

    add model prisma

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import "colors"
 import dotenv from "dotenv"
 import morgan from "morgan"
-
+import path from "path"
 import express from 'express'
 
 import authRoutes from './app/auth/auth.routes.js'
+import userRoutes from './app/user/user.routes.js'
+import exerciseRoutes from './app/exercise/exercise.routes.js'
 import { prisma } from "./app/prisma.js"
 import { errorHandler, notFound } from "./app/middleware/error.middleware.js"
 
@@ -19,7 +21,16 @@ async function main() {
     if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
 	app.use(express.json())
+
+    const __dirname = path.resolve()
+    app.use('/uploads', express.static(path.join(__dirname,'/uploads/')))
+
+
+
 	app.use('/api/auth', authRoutes)
+    app.use('/api/users', userRoutes)
+    app.use('/api/exercises', exerciseRoutes)
+
 
     app.use(notFound)
     app.use(errorHandler)
