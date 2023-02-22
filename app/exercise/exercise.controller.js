208a154dd162a24f44ddf3362e3c965e628a8d45
Artem <a.bychkoff1@yandex.ru>
2023-02-22 14:55:15 +0300

    add update/delete exercises

--- app/exercise/exercise.controller.js
+++ app/exercise/exercise.controller.js
@@ -7,21 +7,66 @@ import { UserFields } from '../utils/user.utils.js'
 // @route   POST /api/exercise
 // @access  Private
 export const createNewExercise = asyncHandler(async (req, res) => {
-	const {name, times, iconPath} = req.body
+	const { name, times, iconPath } = req.body
 
 	const exercise = await prisma.exercise.create({
 		data: {
-			name, times, iconPath
+			name,
+			times,
+			iconPath
 		}
 	})
 	res.json(exercise)
+})
+
+// @desc    update exercise
+// @route   PUT /api/exercise/:id
+// @access  Private
+export const updateExercise = asyncHandler(async (req, res) => {
+	const { name, times, iconPath } = req.body
+
+	try {
+		const exercise = await prisma.exercise.update({
+			where: {
+				id: +req.params.id
+			},
+			data: {
+				name,
+				times,
+				iconPath
+			}
+		})
+
+		res.json(exercise)
+	} catch (error) {
+		res.status(404)
+		throw new Error('Exercise not found!')
+	}
+})
+
+// @desc    Delete exercise
+// @route 	DELETE /api/exercises/:id
+// @access  Private
+export const deleteExercise = asyncHandler(async (req, res) => {
+	try {
+		const exercise = await prisma.exercise.delete({
+			where: {
+				id: +req.params.id
+			}
+		})
+
+		res.json({ message: 'Exercise deleted!' })
+	} catch (error) {
+		res.status(404)
+		throw new Error('Exercise not found!')
+	}
 })
 
 // @desc    GET exercises
 // @route   GET /api/exercises
 // @access  Private
 export const getExercises = asyncHandler(async (req, res) => {
-	const {name, times, iconPath} = req.body
+	const { name, times, iconPath } = req.body
 
 	const exercises = await prisma.exercise.findMany()
 	res.json(exercises)
